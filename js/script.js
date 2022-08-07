6d75c12220c87225865fd11f417c1ebbad7fc146
MLanfear <maxjlanfear@gmail.com>
2022-08-06 19:44:48 -0700

    end of day save

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
+console.log(window.document);
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-  });
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
